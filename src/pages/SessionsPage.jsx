c85dffab73b79c31c74c5f776022a3ce81b713ef
Jacob Espinosa <jaespinosa03@outlook.com>
2026-09-04 11:36:30 -0700

    add feature to limit sessions per page to 50

--- src/pages/SessionsPage.jsx
+++ src/pages/SessionsPage.jsx
@@ -1,12 +1,38 @@
 import './SessionsPage.css';
 import SessionsHistory from '../components/sessions/SessionsHistory';
 import { getSortedSessions } from '../utils/sessionUtils';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { useEffect, useRef, useState } from 'react';
 
 function SessionsPage({sessions, setSessions, tasksByDate, projects, timeByDate,
                        weeklyTimeGoals, handleEditSession
 }) {
     const sortedSessions = getSortedSessions(sessions);
     const isEmpty = sessions.length === 0;
+
+    const SESSIONS_PER_PAGE = 50;
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const startIndex = (currentPage - 1) * SESSIONS_PER_PAGE;
+    const endIndex = startIndex + SESSIONS_PER_PAGE;
+
+    const currentSessions = sortedSessions.slice(startIndex, endIndex);
+
+    const totalPages = Math.ceil(
+        sessions.length / SESSIONS_PER_PAGE
+    );
+
+    function handlePageChange(newPage) {
+        setCurrentPage(newPage);
+
+        const pageContent = document.querySelector(".page-content");
+
+        pageContent?.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    }
 
     return (
         <main className="sessions">
@@ -17,7 +43,7 @@ function SessionsPage({sessions, setSessions, tasksByDate, projects, timeByDate,
                 {isEmpty && <span className='no-session-msg'>No Sessions</span>}
                 <div className="sessions-list">
                     <SessionsHistory 
-                        sessions={sortedSessions} 
+                        sessions={currentSessions} 
                         setSessions={setSessions}
                         tasksByDate={tasksByDate}
                         projects={projects}
@@ -26,6 +52,29 @@ function SessionsPage({sessions, setSessions, tasksByDate, projects, timeByDate,
                         handleEditSession={handleEditSession}
                     />
                 </div>
+                {!isEmpty &&
+                    <div className='page-navigation-container'>
+                        <button type="button" 
+                                className='page-nav-prev'
+                                onClick={() => handlePageChange(currentPage - 1)}
+                                disabled={currentPage === 1}
+                        >
+                            <FontAwesomeIcon icon={faChevronLeft} />
+                        </button>
+
+                        <span className='page-nav-text'>
+                            Page {currentPage} of {totalPages}
+                        </span>
+
+                        <button type="button"
+                                className='page-nav-next'
+                                onClick={() => handlePageChange(currentPage + 1)}
+                                disabled={currentPage === totalPages}
+                        >
+                            <FontAwesomeIcon icon={faChevronRight} />
+                        </button>
+                    </div>
+                }
             </section>
         </main>
     );
